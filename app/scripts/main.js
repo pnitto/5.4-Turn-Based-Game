5550a1c76dacfc2199213935b3344e544b5bf57c
Paul Nitto <pnitto18@gmail.com>
2015-09-24 13:33:31 -0400

    set up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,5 @@ var views = require('views');
 $(document).ready(function(){
   $('body').prepend(JST.application());
 
-  var view = new PostsView();
-
-  $(document).on('posts:fetched', function(event, posts){
-    view.showPosts(posts);
-  });
-
-  Post.fetch();
+alert('hello');
 });
