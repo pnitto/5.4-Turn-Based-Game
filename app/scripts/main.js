01df85e3606943de0a98209a4ffbc1fe4031b6ef
Paul Nitto <pnitto18@gmail.com>
2015-09-24 14:19:11 -0400

    started constructor with methods and player instances

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,5 +4,19 @@ var views = require('views');
 $(document).ready(function(){
   $('body').prepend(JST.application());
 
-alert('hello');
+function Player(character){
+  this.health = 100,
+  this.name = character.name
+}
+Player.prototype.attack = function(attack){
+
+}
+Player.prototype.specialAttack = function(attack){
+}
+
+var scorpion = new Player({name: 'Scorpion'});
+var subZero = new Player({name: 'SubZero'});
+var raiden = new Player({name: 'Raiden'});
+var jaxx = new Player({name: 'Jaxx'});
+
 });
